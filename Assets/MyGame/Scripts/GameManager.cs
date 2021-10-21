d48e98f10ba5ec39ab26176647b53a0a54d78a5b
KMIJAT <90834524+KMIJAT@users.noreply.github.com>
2021-10-21 21:08:44 +0200

    Add files via upload

---
+++ Assets/MyGame/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject target;
+    public GameObject parentOfTargets;
+
+    public bool won;
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        won = false;
+
+        InvokeRepeating("Spawn", 1f, 2f);
+
+    }
+
+    //Spawn a target at a randm position within a specified x and y range.
+    //Instantiate (make a concret GameObject, i.e., a clone from the given prefabs target) the
+    //target as child of the ParentOfTargets. In this case transform.localPosition instead of 
+    //transform.position is important!!
+
+    private void Spawn()
+    {
+        float randomX = Random.Range(-400, 400);
+        float randomY = Random.Range(-150, 150);
+
+        Vector2 random2DPosition = new Vector2(randomX, randomY);
+
+        GameObject myTarget = Instantiate(target, parentOfTargets.transform);
+        myTarget.transform.localPosition = random2DPosition;
+
+        Debug.Log(random2DPosition);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(won == true)
+        {
+            CancelInvoke("Spawn");
+        }
+        else
+        {
+            Debug.Log(won);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse pressed");
+        }
+    }
+
+    public void IncrementScore()
+    {
+        score++;
+        Debug.Log("increment ..." + score);
+
+        if(score > 10)
+        {
+            won = true;
+        }
+    }
+}
---
+++ Assets/MyGame/Scripts/Target.cs
@@ -0,0 +1,25 @@
+
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+
+    [SerializeField] private int secToDestroy;
+    public GameManager gameManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //secToDestroy = 2;
+        //Destroy(gameObject, secToDestroy);
+    }
+
+    private void OnMouseDown()
+    {
+        gameManager.IncrementScore();
+        Destroy(gameObject);
+    }
+
+
+    
+}
